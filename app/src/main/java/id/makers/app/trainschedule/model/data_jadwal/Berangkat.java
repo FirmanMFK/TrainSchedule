359fa36dbf8788ece1409173918ccb87bc5776c4
FirmanMFK <firman.mfk@gmail.com>
2017-04-12 01:13:13 +0700

    update Berangkat

--- app/src/main/java/id/makers/app/trainschedule/model/data_jadwal/Berangkat.java
+++ app/src/main/java/id/makers/app/trainschedule/model/data_jadwal/Berangkat.java
@@ -1,8 +1,32 @@
 package id.makers.app.trainschedule.model.data_jadwal;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Firman on 4/11/2017.
  */
 
 public class Berangkat {
+    @SerializedName("jam")
+    @Expose
+    private String jam;
+    @SerializedName("tanggal")
+    @Expose
+    private String tanggal;
+    public String getJam() {
+        return jam;
+    }
+
+    public void setJam(String jam) {
+        this.jam = jam;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
 }
