b0da3bb444e432642277c65f4b8f92ca760cd230
FirmanMFK <firman.mfk@gmail.com>
2017-04-12 01:39:14 +0700

    update feature

--- app/src/main/java/id/makers/app/trainschedule/model/data_jadwal/Datang.java
+++ app/src/main/java/id/makers/app/trainschedule/model/data_jadwal/Datang.java
@@ -1,8 +1,33 @@
 package id.makers.app.trainschedule.model.data_jadwal;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Firman on 4/12/2017.
  */
 
 public class Datang {
+    @SerializedName("jam")
+    @Expose
+    private String jam;
+    @SerializedName("tanggal")
+    @Expose
+    private String tanggal;
+
+    public String getJam() {
+        return jam;
+    }
+
+    public void setJam(String jam) {
+        this.jam = jam;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
 }
