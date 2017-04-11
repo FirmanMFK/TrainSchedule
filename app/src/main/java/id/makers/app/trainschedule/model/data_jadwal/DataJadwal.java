4fef81220775c8b6654b1c867af974ba72d55433
FirmanMFK <firman.mfk@gmail.com>
2017-04-12 01:34:38 +0700

    update data jadwal

--- app/src/main/java/id/makers/app/trainschedule/model/data_jadwal/DataJadwal.java
+++ app/src/main/java/id/makers/app/trainschedule/model/data_jadwal/DataJadwal.java
@@ -1,8 +1,46 @@
 package id.makers.app.trainschedule.model.data_jadwal;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by Firman on 4/12/2017.
  */
 
 public class DataJadwal {
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("view")
+    @Expose
+    private String view;
+    @SerializedName("data")
+    @Expose
+    private List<Datum> data = null;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+
+    public List<Datum> getData() {
+        return data;
+    }
+
+    public void setData(List<Datum> data) {
+        this.data = data;
+    }
 }
---
+++ app/src/main/java/id/makers/app/trainschedule/model/data_stasiun/Tanggal.java
@@ -0,0 +1,8 @@
+package id.makers.app.trainschedule.model.data_stasiun;
+
+/**
+ * Created by Firman on 4/12/2017.
+ */
+
+public class Tanggal {
+}
