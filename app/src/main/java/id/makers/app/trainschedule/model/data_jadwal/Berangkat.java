a397a06b047d8d6ff0cde9bc94b4f909e2a3b92d
FirmanMFK <firman.mfk@gmail.com>
2017-04-12 01:01:44 +0700

    Update Model

---
+++ app/src/main/java/id/makers/app/trainschedule/model/data_jadwal/Berangkat.java
@@ -0,0 +1,8 @@
+package id.makers.app.trainschedule.model.data_jadwal;
+
+/**
+ * Created by Firman on 4/11/2017.
+ */
+
+public class Berangkat {
+}
---
+++ app/src/main/java/id/makers/app/trainschedule/model/data_jadwal/DataJadwal.java
@@ -0,0 +1,8 @@
+package id.makers.app.trainschedule.model.data_jadwal;
+
+/**
+ * Created by Firman on 4/12/2017.
+ */
+
+public class DataJadwal {
+}
