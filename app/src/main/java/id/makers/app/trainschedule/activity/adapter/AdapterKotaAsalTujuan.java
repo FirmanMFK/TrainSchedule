e5a2dec866b84037022f7ccc1dc6aa38c9151b80
FirmanMFK <firman.mfk@gmail.com>
2017-04-12 22:47:10 +0700

    tambah Adapter , Home dan Main

--- app/src/main/java/id/makers/app/trainschedule/activity/adapter/AdapterKotaAsalTujuan.java
+++ app/src/main/java/id/makers/app/trainschedule/activity/adapter/AdapterKotaAsalTujuan.java
@@ -1,8 +1,27 @@
 package id.makers.app.trainschedule.activity.adapter;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
 /**
  * Created by Firman on 4/11/2017.
  */
 
-public class AdapterKotaAsalTujuan {
+public class AdapterKotaAsalTujuan extends RecyclerView.Adapter {
+    private static final String TAG = "AdapterTAG";
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
